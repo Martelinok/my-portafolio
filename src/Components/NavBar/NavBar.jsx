f31599f48c5df14fd7584a32d6dcc2cb5cef483b
Kevin Martello <kevinmartello@icloud.com>
2022-02-21 15:09:39 -0500

    Implement About me

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -35,7 +35,7 @@ function NavBar({ image, Home, Services, Projects, AboutMe, Contact, dispatch, L
             </div>
             <div className="SideNavbar_Options">
               <p className={`SideNavbar_Options_Text ${navigation === "Home" ? true : false}`} onClick={() => onClickOption('Home')}>{Home}</p>
-              <p className={`SideNavbar_Options_Text ${navigation === "Services" ? true : false}`} onClick={() => onClickOption('Services')}>{Services}</p>
+              {/* <p className={`SideNavbar_Options_Text ${navigation === "Services" ? true : false}`} onClick={() => onClickOption('Services')}>{Services}</p> */}
               <p className={`SideNavbar_Options_Text ${navigation === "Projects" ? true : false}`} onClick={() => onClickOption('Projects')}>{Projects}</p>
               <p className={`SideNavbar_Options_Text ${navigation === "AboutMe" ? true : false}`} onClick={() => onClickOption('AboutMe')}>{AboutMe}</p>
               <p className={`SideNavbar_Options_Text ${navigation === "Contact" ? true : false}`} onClick={() => onClickOption('Contact')}>{Contact}</p>
@@ -52,7 +52,7 @@ function NavBar({ image, Home, Services, Projects, AboutMe, Contact, dispatch, L
           </div>
           <div className="Navbar_Options_Content">
             <p className={`NavBar_Options_Text ${navigation === "Home" ? true : false}`} onClick={() => onClickOption('Home')}>{Home}</p>
-            <p className={`NavBar_Options_Text ${navigation === "Services" ? true : false}`} onClick={() => onClickOption('Services')}>{Services}</p>
+            {/* <p className={`NavBar_Options_Text ${navigation === "Services" ? true : false}`} onClick={() => onClickOption('Services')}>{Services}</p> */}
             <p className={`NavBar_Options_Text ${navigation === "Projects" ? true : false}`} onClick={() => onClickOption('Projects')}>{Projects}</p>
             <p className={`NavBar_Options_Text ${navigation === "AboutMe" ? true : false}`} onClick={() => onClickOption('AboutMe')}>{AboutMe}</p>
             <p className={`NavBar_Options_Text ${navigation === "Contact" ? true : false}`} onClick={() => onClickOption('Contact')}>{Contact}</p>
--- src/Page/AboutMe/AboutMe.jsx
+++ src/Page/AboutMe/AboutMe.jsx
@@ -1,9 +1,47 @@
 import React from "react";
-
+import { useTranslate } from "react-translate";
+/* ---------------------------- Import Components --------------------------- */
+import Kcm from "../../assets/Images/Kcm-Layout.png";
+import Skills from "../../assets/Images/Skills.png";
+/* ------------------------------ Import Style ------------------------------ */
+import './AboutMe.css';
 function AboutMe(params) {
+    const t = useTranslate("AboutMe");
     return (
         <React.Fragment>
-            <h1>AboutMe</h1>
+            <div className="AboutMe_Container">
+                <div className="AboutMe_Content">
+                    <div className="AboutMe_Text_Content">
+                        <div className="AboutMe_Text_Title_Content">
+                            <div className="AboutMe_Title_Line"></div>
+                            <h3 className="AboutMe_Text_Title">{t("AboutMe")}</h3>
+                        </div>
+                        <div className="AboutMe_Text_Big_Title_Content">
+                            <h1 className="AboutMe_Text_Big_Title_H1">{t("AboutMeTitle")}</h1>
+                        </div>
+                        <div className="AboutMe_Text_P_Content">
+                            <p className="AboutMe_Text_P">{t("AboutMeText")}</p>
+                        </div>
+                    </div>
+                    <div className="AboutMe_Image_Content">
+                        <img src={Kcm} alt="" className="AboutMe_Image_Pair_Style" />
+                    </div>
+                </div>
+                <div className="AboutMe_MySkills_Content">
+                    <div className="MySkills_Image_Content">
+                        <img src={Skills} alt="" className="MySkills_Image_Style" />
+                    </div>
+                    <div className="MySkills_Text_Content">
+                        <div className="MySkills_Title_Content">
+                            <div className="MySkills_Title_Line"></div>
+                            <h3 className="MySkills_Text_Title">{t("MySkills")}</h3>
+                        </div>
+                        <div className="MySkills_Text_P_Content">
+                            <p className="MySkills_Text_P">{t("MySkillsText")}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </React.Fragment>
     )
 }
